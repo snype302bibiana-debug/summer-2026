cce800abde682eb9219d91fca1ca5f84834ca1b4
snype302bibiana-debug <snype302bibiana@gmail.com>
2026-06-02 12:34:49 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'summer-2026-v1'
+const ASSETS = ['/', '/index.html']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
